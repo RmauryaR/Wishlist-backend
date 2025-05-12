c8a57977c3bfcd5675aedc36ce0aa9fdcb0f0566
Creator-G-star <78673793+RmauryaR@users.noreply.github.com>
2025-05-12 16:16:00 +0530

    Added start script for deployment

--- index.js
+++ index.js
@@ -26,10 +26,7 @@ app.get("/", (req, res) => {
 const PORT = process.env.PORT || 5000;
 
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("MongoDB connected");
     app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
